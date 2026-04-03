50f24e29946ba9a8abd00c7ea64d9f865a8ecfb5
SenSive <alejandrovalenzuelax@icloud.com>
2026-04-03 17:18:17 -0600

    feat: add patient model linked to optic

--- Opticore_backend/apps/patients/models.py
+++ Opticore_backend/apps/patients/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
+from apps.optics.models import Optic
 
-# Create your models here.
+class Patient(models.Model):
+    optic = models.ForeignKey(
+        Optic,
+        on_delete=models.CASCADE,
+        related_name="patients"
+    )
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    phone = models.CharField(max_length=30, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    birth_date = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=20, blank=True, null=True)
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="created_patients"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
